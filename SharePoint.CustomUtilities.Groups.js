0bb2791b0d9cbb6553411a78b3cbecdd195734a4
Ian Chursky <ianchursky@gmail.com>
2015-12-07 16:11:29 -0800

    Added a getSitePeopleAndGroups method...

--- SharePoint.CustomUtilities.Groups.js
+++ SharePoint.CustomUtilities.Groups.js
@@ -25,6 +25,38 @@ SharePoint.CustomUtilities.Groups = {
             console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
         });          
         
-    }
+    },
+    getSitePeopleAndGroups: function(){
+        var clientContext = new SP.ClientContext.get_current();
+        var web = clientContext.get_web();
+        var userList = web.get_siteUserInfoList();
+        var query = new SP.CamlQuery(); // object is a required parameter for this query
+        var items = userList.getItems(query);
+        clientContext.load(items);
+
+        clientContext.executeQueryAsync(function(sender, args) {
+            var itemEnumerator = items.getEnumerator();
+            var itemArray = [];
+            while (itemEnumerator.moveNext()) {
+                var item = itemEnumerator.get_current();
+
+                //console.log(item.get_fieldValues()); // displays all the proerties of list item
+                
+                var data = {
+                    'ID': item.get_id(),
+                    'Title': item.get_item('Title'),
+                    'UserName': item.get_item('UserName'),
+                    'IsActive': item.get_item('IsActive'),                    
+                    'IsSiteAdmin': item.get_item('IsSiteAdmin')
+                };
+                itemArray.push(data);            
+            }
+
+            return itemArray;
+                        
+        }, function(sender, args) {
+            console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+        });    
+    }    
     
 };
