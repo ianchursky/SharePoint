d35e13deab2e1ef197367ad6c1528e62afc00f71
Ian Chursky <ianchursky@gmail.com>
2016-05-28 07:08:20 -0700

    Separating custom action scopes into separate methods...

--- SharePoint.CustomUtilities.js
+++ SharePoint.CustomUtilities.js
@@ -3,10 +3,10 @@ SharePoint.CustomUtilities = SharePoint.CustomUtilities || {};
 
 SharePoint.CustomUtilities.CustomActions = {
     
-    getAllCustomActions: function(scope){
+    getAllSiteCustomActions: function(){
         SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
             var clientContext = SP.ClientContext.get_current();
-            var web = scope === 'site' ? clientContext.get_site() : clientContext.get_web();
+            var web = clientContext.get_site();
             var customActions = web.get_userCustomActions();
             var customActionArray = [];
             clientContext.load(customActions);
@@ -25,10 +25,10 @@ SharePoint.CustomUtilities.CustomActions = {
             });
         });                
     },       
-    setCustomAction: function(name, description, url, sequence, scope){
+    setSiteCustomAction: function(name, description, url, sequence){
         SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
             var clientContext = SP.ClientContext.get_current();
-            var web = scope === 'site' ? clientContext.get_site() : clientContext.get_web();
+            var web = clientContext.get_site();
             var customActions = web.get_userCustomActions();
             clientContext.load(customActions);
             clientContext.executeQueryAsync(function () {
@@ -43,11 +43,11 @@ SharePoint.CustomUtilities.CustomActions = {
             });
         })                
     },
-    removeCustomAction: function(name, scope) {
+    removeSiteCustomAction: function(name) {
         
         SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
             var clientContext = SP.ClientContext.get_current();
-            var web = scope === 'site' ? clientContext.get_site() : clientContext.get_web();
+            var web = clientContext.get_site();
             var customActions = web.get_userCustomActions();
 
             var customAction;
@@ -64,7 +64,69 @@ SharePoint.CustomUtilities.CustomActions = {
                 clientContext.executeQueryAsync();
             });
         });        
-    }    
+    },
+    getAllWebCustomActions: function(){
+        SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+            var clientContext = SP.ClientContext.get_current();
+            var web = clientContext.get_web();
+            var customActions = web.get_userCustomActions();
+            var customActionArray = [];
+            clientContext.load(customActions);
+            clientContext.executeQueryAsync(function () {
+                var customActionEnumerator = customActions.getEnumerator();
+                while (customActionEnumerator.moveNext()) {
+                    var currentCustomAction = customActionEnumerator.get_current();
+                    var data = {
+                        Title: currentCustomAction.get_title(),
+                        Description: currentCustomAction.get_description(),
+                        URL: currentCustomAction.get_scriptSrc()
+                    };
+                    console.log(data);
+                    customActionArray.push(data);
+                }
+            });
+        });                
+    },       
+    setWebCustomAction: function(name, description, url, sequence){
+        SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+            var clientContext = SP.ClientContext.get_current();
+            var web = clientContext.get_web();
+            var customActions = web.get_userCustomActions();
+            clientContext.load(customActions);
+            clientContext.executeQueryAsync(function () {
+                customAction = customActions.add();
+                customAction.set_title(name);
+                customAction.set_location("ScriptLink");
+                customAction.set_description(description || "No Description");
+                customAction.set_scriptSrc(url);
+                customAction.set_sequence(sequence || 100);
+                customAction.update();
+                clientContext.executeQueryAsync();
+            });
+        })                
+    },
+    removeWebCustomAction: function(name) {
+        
+        SP.SOD.executeFunc('sp.js', 'SP.ClientContext', function () {
+            var clientContext = SP.ClientContext.get_current();
+            var web = clientContext.get_web();
+            var customActions = web.get_userCustomActions();
+
+            var customAction;
+            clientContext.load(customActions);
+            clientContext.executeQueryAsync(function () {
+                var customActionEnumerator = customActions.getEnumerator();
+                while (customActionEnumerator.moveNext()) {
+                    var currentCustomAction = customActionEnumerator.get_current();
+                    if (currentCustomAction.get_title() == name) {
+                        customAction = currentCustomAction;
+                    }
+                }
+                customAction.deleteObject();
+                clientContext.executeQueryAsync();
+            });
+        });        
+    }           
         
 };
 var SharePoint = SharePoint || {};
