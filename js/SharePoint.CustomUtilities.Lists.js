f9e4dfd0d28340a0f26012d2a9879e6db72b6db4
Ian Chursky <ianchursky@gmail.com>
2017-05-23 15:50:42 -0700

    Adding methods to create and delete lists... Adding event receiver methods...

--- js/SharePoint.CustomUtilities.Lists.js
+++ js/SharePoint.CustomUtilities.Lists.js
@@ -106,6 +106,22 @@ SharePoint.CustomUtilities.Lists = {
             console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
         });                 
     },
+    createListItem: function(siteUrl, listTitle, valuesArray) {
+        var clientContext = new SP.ClientContext(siteUrl);
+        var list = clientContext.get_web().get_lists().getByTitle(listTitle);
+        var itemCreateInfo = new SP.ListItemCreationInformation();
+        var listItem = list.addItem(itemCreateInfo);
+        valuesArray.forEach(function(item, index){
+            listItem.set_item(item["Title"], item["Value"]);
+        });
+        listItem.update();
+        clientContext.load(listItem);            
+        clientContext.executeQueryAsync(function(sender, args){
+            console.log('Success');
+        }, function(sender, args){
+            console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+        });
+    },
     deleteListItem: function(listName, itemName) {
         var clientContext = new SP.ClientContext.get_current();
         var web = clientContext.get_web();
@@ -136,6 +152,45 @@ SharePoint.CustomUtilities.Lists = {
             console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
         });                 
     }, 
+
+    createList: function(listName, templateType) {
+
+        var def = $.Deferred();
+        var clientContext = new SP.ClientContext.get_current();
+        var web = clientContext.get_web();
+        var listCreationInfo = new SP.ListCreationInformation();
+        listCreationInfo.set_title(listName);
+        listCreationInfo.set_templateType(templateType || SP.ListTemplateType.genericList);
+        var list = web.get_lists().add(listCreationInfo);
+        var defaultView = list.get_defaultView();
+        defaultView.get_viewFields().add("Created");
+        defaultView.update();
+
+        list.get_fields().addFieldAsXml("<Field Type='URL' Name='SiteUrl' DisplayName='Site Url' StaticName='SiteUrl' Required='TRUE' Format='Hyperlink' />", true, SP.AddFieldOptions.addFieldInternalNameHint);
+        list.get_fields().addFieldAsXml("<Field Type='Choice' Name='SiteTemplate' DisplayName='Site Template' StaticName='SiteTemplate' Required='TRUE' Format='Dropdown'>"
+        + "<Default>BLANKINTERNETCONTAINER#0</Default>"
+        + "<CHOICES>"
+        + "    <CHOICE>BLANKINTERNETCONTAINER#0</CHOICE>"
+        + "    <CHOICE>STS#0</CHOICE>"
+        + "    <CHOICE>DEV#0</CHOICE>"
+        + "</CHOICES>"
+        + "</Field>", true, SP.AddFieldOptions.addFieldInternalNameHint);
+
+        clientContext.load(list);
+        clientContext.executeQueryAsync(function () { }, function () { });
+    },    
+
+    deleteList: function (listName) {
+        var def = $.Deferred();
+        var clientContext = new SP.ClientContext.get_current();
+        var web = clientContext.get_web();
+        var list = web.get_lists().getByTitle(listName);
+        list.deleteObject();
+        clientContext.executeQueryAsync(function () {
+        }, function () {
+        });
+    },
+
     getImageRenditions: function(){
         var clientContext = new SP.ClientContext.get_current();
         var renditions = SP.Publishing.SiteImageRenditions.getRenditions(clientContext);
@@ -159,7 +214,59 @@ SharePoint.CustomUtilities.Lists = {
         }, function(sender, args){
             console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
         });        
-    }       
+    },
+
+    addEventReceiversToList: function(listName, name, url) {
+        var context = new SP.ClientContext();
+        var web = context.get_web();
+        var list = web.get_lists().getByTitle(listName);
+        var receiver = new SP.EventReceiverDefinitionCreationInformation();
+        receiver.set_receiverName(name)
+        receiver.set_sequenceNumber(100);
+        receiver.set_receiverUrl(url);
+        receiver.set_eventType(SP.EventReceiverType.itemAdded); // SP.EventReceiverType.itemAdded = 10001 = Item added
+        receiver.set_synchronization(SP.EventReceiverSynchronization.synchronous);
+        list.get_eventReceivers().add(receiver);
+        context.load(list);
+        context.executeQueryAsync(function(sender, args) {
+            console.log('Event receiver added...')
+        }, function(sender, args){
+            console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+        });
+
+    },
+
+    getEventReceiversForList: function(listName) {
+        var context = new SP.ClientContext();
+        var web = context.get_web();
+        var list = web.get_lists().getByTitle(listName);
+        var eventReceivers = list.get_eventReceivers();
+        context.load(eventReceivers);
+
+        context.executeQueryAsync(function(sender, args) {
+            var eventReceiverArray = [];
+            for(var i=0; i < eventReceivers.get_count(); i++) {
+                var eventReceiver = eventReceivers.get_item(i);
+                var item = {
+                    "ID": eventReceiver.get_receiverId(),
+                    "Name": eventReceiver.get_receiverName(),
+                    "Path": eventReceiver.get_path(),
+                    "EventType": eventReceiver.get_eventType(),
+                    "Url": eventReceiver.get_receiverUrl(),
+                    "Synchronization": eventReceiver.get_synchronization(),
+                    "Class": eventReceiver.get_receiverClass(),
+                    "Assembly": eventReceiver.get_receiverAssembly(),
+                    "SequenceNumber": eventReceiver.get_sequenceNumber()  
+                };
+                eventReceiverArray.push(item);
+            };
+
+            console.log(eventReceiverArray);
+
+        }, function(sender, args){
+            console.error('Request failed. ' + args.get_message() + '\n' + args.get_stackTrace());
+        });
+    }           
 };
 
 
